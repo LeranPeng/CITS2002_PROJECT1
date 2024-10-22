a3c1d5169f68d273833afbcab602a824b31bd1ad
Leran23 <143847694+LeranPeng@users.noreply.github.com>
2024-10-22 16:52:41 +0800

    Delete includes directory

--- includes/parser.h
+++
@@ -1,6 +0,0 @@
-#ifndef _PARSER_H
-#define _PARSER_H
-
-char *parser(char **input_lines);
-
-#endif
--- includes/symbol_table.h
+++
@@ -1,6 +0,0 @@
-#ifndef _SYMBOL_TABLE_H
-#define _SYMBOL_TABLE_H
-
-void symbol_table_start(char *input_lines);
-
-#endif
